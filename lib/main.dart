3682530d020a73ed7193374b7aa3ad4b84b9e57e
JhayR <zeuskaizen17@gmail.com>
2023-10-27 16:16:10 +0800

    bulk commit for the todo app

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+// import 'package:todo_app/ui_updates_demo.dart';
+import 'package:todo_app/keys/keys.dart';
+
+void main() {
+  final numbers = [1, 2, 3];
+  numbers.add(4);
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Internals'),
+        ),
+        body: const Keys(),
+      ),
+    );
+  }
+}
